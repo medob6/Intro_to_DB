3a838bfa00451cc41b8d7434230b88e60b13a451
medob6 <mohamedboussetta142@gmail.com>
2024-08-09 19:04:47 +0100

    my first sql schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,34 +4,38 @@ CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
 CREATE TABLE Books (
-    book_id (Primary Key),
-    title VARCHAR(130),
-    author_id (Foreign Key referencing Authors table),
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
     price DOUBLE,
-    publication_date DATE
+    publication_date DATE,
+    Foreign Key (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Authors (
-    author_id (Primary Key),
-    author_name VARCHAR(215)
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
 CREATE TABLE Customers (
-    customer_id (Primary Key),
-    customer_name VARCHAR(215),
-    email VARCHAR(215),
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
     address TEXT
 );
 
 CREATE TABLE Orders (
-    order_id (Primary Key),
-    customer_id (Foreign Key referencing Customers table),
-    order_date DATE
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details(
-    orderdetailid (Primary Key),
-    order_id (Foreign Key referencing Orders table),
-    book_id (Foreign Key referencing Books table),
-    quantity DOUBLE
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
