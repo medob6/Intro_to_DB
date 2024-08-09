43c8c7380f5f56c563176f669f569d564af4ee26
medob6 <mohamedboussetta142@gmail.com>
2024-08-09 22:14:32 +0100

    Inserting to a table

--- task_5.sql
+++ task_5.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO Customers (customer_id,customer_name,email,address)
+INSERT INTO customer (customer_id,customer_name,email,address)
 VALUES
 ( 1, Cole Baido ,cbaidoo@sandtech.com, 123 Happiness Ave.);
